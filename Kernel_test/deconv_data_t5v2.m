2da71a8338d4bbf2dc939a5d92edd23d3885dd69
MichaelNg1 <mn2769@columbia.edu>
2017-06-19 15:24:30 -0400

    Per Dot Sparse

--- Kernel_test/deconv_data_t5v2.m
+++ Kernel_test/deconv_data_t5v2.m
@@ -10,56 +10,54 @@ addpath('../Chem_microscopy_code');
 range = 1:samples_num;
 % kernel = @lpsf_semi;     % Kernel used
 kernel = @lpsf;
-p_len = 5;                  % number of kernel parameters
-p_eps = 0.06;
-niter = 200;      
+p_len = 4;                  % number of kernel parameters
+niter = 150;      
 LAMBDA_SCALE = 0.5;
 
 
 
 %-Take a single line as truth
-Y = RY(:,1)';
+Y = RY(:,2);
 
 %- Initialize variables:
+x = 0.05 * ones(length(range), 1);
 p_shape = [.05, 2, -3];
-p_scale = [.05];
 
 %-Use 'automatic' parameters:
 for i = 1:samples_num
-    p_task(i,:) = [p_shape, range(i), p_scale];
+    p_task(i,:) = [p_shape, range(i)];
 end
-% p_task(2,4) = 64;
 
 %-Gradient Step size + Jacobian differential
-tp_scale = 0.1 * ones(1, size(p_task,2));
+tp_scale = 1 * ones(1, size(p_task,2));
 tp_scale(4) = 0;
-tp_scale(5) = 0.1;
 
 dp = 0.01 * ones(1, size(p_task,2)); 
 
-tx = 0.05;
+tx = 0.001;
 lambda = LAMBDA_SCALE*max(max(abs(Y)));
 
 %-Functions to generate observation and objective
-objective = @(Yhat, Y, p) 0.5*norm(Yhat - Y,2)^2 + lambda * norm(p(:,5),1);
+objective = @(Yhat, Y, x, lambda) 0.5*norm(Yhat - Y,2)^2 + lambda * norm(x,1);
 
 %% (TASK 4) Estimate p %%
 % For this task the objective function used will be:
 % min 0.5 * || D(p) * x - y ||_2 ^2 + lambda ||x||_1
 %       - d: kernel function
 Yhat = zeros(1,samples_num);
+Yhat_mat = zeros(samples_num, samples_num);
+for j = 1:samples_num
+    Yhat_mat(j,:) = kernel(p_task(j,:), range);
+end
+Yhat = Yhat_mat' * x;
+
 error = zeros(1,niter);
-scale_step = zeros(1,niter);
+lambda_hist = zeros(1,niter);
+tx_hist = zeros(1,niter);
 for i = 1:niter
-    Yhat = zeros(1,samples_num);
-    Yhat_mat = zeros(samples_num, samples_num);
-    for j = 1:samples_num
-        Yhat_mat(j,:) = kernel(p_task(j,:), range);
-        Yhat = Yhat + Yhat_mat(j,:);
-    end
 
     %Update Parameters
-    lambda = LAMBDA_SCALE*max(max(abs(Yhat)));
+    lambda = LAMBDA_SCALE*max(max(abs(Yhat_mat * Y)));
     
     if (mod(i,3) == 0)
         Z = randn(samples_num,1);
@@ -68,13 +66,12 @@ for i = 1:niter
             Z = (Yhat_mat * (Yhat_mat' * Z));
             Z = Z/norm(Z,'fro'); 
         end
-        Lip = 2*norm(Yhat_mat * (Yhat_mat' * Z),'fro');
-        tx = 0.0002/Lip;
-        tp_scale(5) = tx;
-        
+        Lip = norm(Yhat_mat * (Yhat_mat' * Z),'fro');
+        tx = 0.5/Lip;
     end
-    scale_step(i) = lambda;
-    e = objective(Yhat, Y, p_task);
+    lambda_hist (i) = lambda;
+    tx_hist (i) = tx;
+    e = objective(Yhat, Y, x, lambda);
     error(i) = e;
     
     %Estimate Jacobian
@@ -84,10 +81,12 @@ for i = 1:niter
             ek = zeros(size(p_task));
             ek(j,k) = 1;
 
-            Yhat_eps = Yhat + kernel(p_task(j,:) + dp.*ek(j,:) , range) ...
-                        - kernel(p_task(j,:), range);
+            Yhat_mat_eps = Yhat_mat;
+            Yhat_mat_eps(j,:) = kernel(p_task(j,:) + dp.*ek(j,:) , range);
+            Yhat_eps = Yhat_mat_eps' * x;
+
             Jp{j,k} = (Yhat_eps - Yhat) / dp(k);
-            delta(j,k) = (tp_scale(k)/(norm(Jp{j,k},'fro')))*sum(sum(Jp{j,k}.*(Yhat - Y)));
+            delta(j,k) = (tp_scale(k)/(norm(Jp{j,k},'fro')))*sum(sum(x(j) * Jp{j,k}.*(Yhat - Y)));
         end
     end
     
@@ -95,27 +94,31 @@ for i = 1:niter
     %Gradient step in p
     for j = 1:size(p_task,1)
         for k = 1:p_len
-            if k == 5
-                p_task(j,k) = p_task(j,k) - tp_scale(k) * sum(sum(Jp{j,k}.*(Yhat - Y)));
-                p_task(j,k) = sign(p_task(j,k)).*max(abs(p_task(j,k))-lambda*tx,0);
-            else
-                p_task(j,k) = p_task(j,k) - delta(j,k);
-            end
+            p_task(j,k) = p_task(j,k) - delta(j,k);
         end
     end
    
+   %Gradient step in x
+    for j = 1:samples_num
+        Yhat_mat(j,:) = kernel(p_task(j,:), range);
+    end
+    Yhat = Yhat_mat' * x;
+    x = x - tx * Yhat_mat * (Yhat - Y);
+    x = sign(x).*max(abs(x - lambda*tx), 0);
+
     %Project onto correct subspace ASSUMING LPSF(_semi)
-    p_task(:,1) = max(1e-3, p_task(:,1));
-    p_task(:,2) = max(1e-3, p_task(:,2));
-    p_task(:,3) = min(-1e-3, p_task(:,3));
-    p_task(:,4) = max(1e-3, p_task(:,4));
-    p_task(:,5) = max(1e-12, p_task(:,5));
+    p_task(:,1) = max(1e-12, p_task(:,1));
+    p_task(:,2) = max(1e-12, p_task(:,2));
+    p_task(:,3) = min(-1e-12, p_task(:,3));
+    p_task(:,4) = max(1e-12, p_task(:,4));
+    x = max(1e-12, x);
+
 
     disp(['==== Number of iterations :', num2str(i), ' ====']);
     disp(['Objective: ', num2str(e)]);
     for j = 1:size(p_task,1)
-        if(p_task(j,5) > 1e-5)
-            disp(['p_test (',num2str(j),'): ', num2str(p_task(j,:))]);
+        if(x(j) > 1e-2)
+            disp(['p_test (',num2str(j),'): ', num2str(p_task(j,:)),'   ', num2str(x(j))]);
         end
     end
 end
@@ -132,13 +135,16 @@ title('Truth vs. Learned');
 hold off;
 
 subplot(4,1,2);
-plot(scale_step);
+hold on;
+plot(lambda_hist);
+plot(tx_hist);
+hold off;
 xlabel('Number of Iterations');
 ylabel('Step Value');
 title('Step Scale');
 
 subplot(4,1,3);
-stem(p_task(:,5));
+stem(x);
 xlabel('Magnitude');
 ylabel('Location');
 title('Learned Activation Map');
